82f9477da5d1d658f9fd26fdaba1756bbf92f056
bakkerbm <thejamminben@gmail.com>
2019-07-19 03:23:40 -0400

    test commit from vscode

--- race scraper.py
+++ race scraper.py
@@ -67,16 +67,16 @@ while completed < racesToScrape:
 
 # common words to exclude from unique mistake list
 commonWordList = ['the', 'be', 'to', 'and', 'a', 'in', 'that', 'have', 'I', 'it',
-                     'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 
-                     'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 
-                     'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 
-                     'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me', 
-                     'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 
-                     'take', 'person', 'into', 'year', 'your', 'good', 'some', 'could', 
-                     'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 
-                     'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 
-                     'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'any', 
-                     'these', 'give', 'day', 'most', 'us', 'of', 'saw', 'many']
+            'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 
+            'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 
+            'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 
+            'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me', 
+            'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 
+            'take', 'person', 'into', 'year', 'your', 'good', 'some', 'could', 
+            'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 
+            'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 
+            'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'any', 
+            'these', 'give', 'day', 'most', 'us', 'of', 'saw', 'many']
 
 ## record results in files
 with open('raceresults.txt', 'r+') as resultFile:
